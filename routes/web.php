4d9ceb8c4282e22f915f06665bedb5d2f497824b
Zsolt <fzs@fwmail.hu>
2018-02-15 08:15:54 +0000

    CS fix

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,6 @@ Route::get('/', 'HomeController@index');
 
 Route::get('/blog', 'BlogController@index');
 Route::get('/blog/create', 'BlogController@create');
-
 Route::post('blog', 'BlogController@store');
-
 Route::get('/blog/edit/{id}', 'BlogController@edit');
 Route::post('/blog/edit/{id}', 'BlogController@update');
